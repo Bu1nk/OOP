d97c2f0df1c224670091631dfde276e3b5696ec1
Abramkin Nikita <nik.abramkin05@gmail.com>
2024-09-10 11:15:06 +0700

    sample

---
+++ Task_1_1_1/src/test/java/ru/nsu/abramkin/MainTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.abramkin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void SampleTest() {
+        assertEquals( expected 3, Main.sum( a: 1, b: 2));
+    }
+}
