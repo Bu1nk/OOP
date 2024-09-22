f118571b14058fffc22f0b09411ee1cbe6e37086
Nikita Abramkin <n.abramkin@g.nsu.ru>
2024-09-22 20:54:16 +0700

    sample

--- Task_1_1_1/src/test/java/ru/nsu/abramkin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/abramkin/MainTest.java
@@ -11,4 +11,12 @@ class MainTest {
     void SampleTest() {
         assertArrayEquals(sarr, Main.heapsort(arr));
     }
+
+    int arr1[] = {12, 11, 11, 5, 6, 7};
+    int sarr1[] = {5, 6, 7, 11, 11, 12};
+
+    @Test
+    void SameTest() {
+        assertArrayEquals(sarr1, Main.heapsort(arr1));
+    }
 }
