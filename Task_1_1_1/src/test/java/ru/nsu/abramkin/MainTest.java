a68d3564aeec89a4ca46d9cd334e9200e8347ff0
Nikita Abramkin <n.abramkin@g.nsu.ru>
2024-10-03 09:29:19 +0700

    tests

--- Task_1_1_1/src/test/java/ru/nsu/abramkin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/abramkin/MainTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class MainTest {
 
     /**
-     * Тест для сортировки массива в обратном порядке.
-     * Ожидаемый результат: массив будет отсортирован по возрастанию.
+     * Тест для сортировки уже отсортированного массива.
+     * Ожидаемый результат: массив останется отсортированным.
      */
     @Test
     public void testSortedArray() {
@@ -20,8 +20,8 @@ class MainTest {
     }
 
     /**
-     * Тест для сортировки уже отсортированного массива.
-     * Ожидаемый результат: массив останется отсортированным.
+     * Тест для сортировки массива в обратном порядке.
+     * Ожидаемый результат: массив будет отсортирован по возрастанию.
      */
     @Test
     public void testReversedArray() {
@@ -42,8 +42,7 @@ class MainTest {
     }
 
     /**
-     * Тест для сортировки массива с одним элементом.
-     * Ожидаемый результат: массив останется неизменным.
+     * Тест для сортировки массива с повторяющимся элементом.
      */
     @Test
     public void testArrayWithDuplicates() {
@@ -61,5 +60,46 @@ class MainTest {
         int[] arr = {};
         Heapsort.heapsort(arr);
         assertArrayEquals(new int[] {}, arr);
+    }
+
+    /**
+     * Тест для сортировки массива с одним элементом.
+     * Ожидаемый результат: массив останется неизменным.
+     */
+    @Test
+    public void testSingleElementArray() {
+        int[] arr = {42};
+        Heapsort.heapsort(arr);
+        assertArrayEquals(new int[] {42}, arr);
+    }
+
+    /**
+     * Тест для сортировки массива с отрицательными числами.
+     * Ожидаемый результат: массив будет отсортирован по возрастанию.
+     */
+    @Test
+    public void testArrayWithNegativeNumbers() {
+        int[] arr = {-1, -3, 4, -2, 0};
+        Heapsort.heapsort(arr);
+        assertArrayEquals(new int[] {-3, -2, -1, 0, 4}, arr);
+    }
+
+    /**
+     * Тест для сортировки большого массива.
+     * Ожидаемый результат: массив будет корректно отсортирован.
+     */
+    @Test
+    public void testLargeArray() {
+        int[] arr = new int[1000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1000 - i;
+        }
+        Heapsort.heapsort(arr);
+
+        int[] expected = new int[1000];
+        for (int i = 0; i < expected.length; i++) {
+            expected[i] = i + 1;
+        }
+        assertArrayEquals(expected, arr);
     }
 }
