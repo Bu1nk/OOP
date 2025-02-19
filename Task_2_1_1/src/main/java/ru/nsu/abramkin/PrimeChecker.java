f5f9d9020caceec1f1217508c68b3a86dbfee358
Nikita Abramkin <n.abramkin@g.nsu.ru>
2025-02-19 11:24:38 +0700

    description of schedule

--- Task_2_1_1/src/main/java/ru/nsu/abramkin/PrimeChecker.java
+++ Task_2_1_1/src/main/java/ru/nsu/abramkin/PrimeChecker.java
@@ -16,7 +16,9 @@ public class PrimeChecker {
     private static boolean isPrime(int num) {
         if (num < 2) return false;
         for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) return false;
+            if (num % i == 0) {
+                return false;
+            }
         }
         return true;
     }
