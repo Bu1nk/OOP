126de6f3fa77048248179f10fa4bd8363a642b29
Nikita Abramkin <n.abramkin@g.nsu.ru>
2025-02-19 11:55:28 +0700

    improve checkstyle3

--- Task_2_1_1/src/main/java/ru/nsu/abramkin/PrimeChecker.java
+++ Task_2_1_1/src/main/java/ru/nsu/abramkin/PrimeChecker.java
@@ -3,9 +3,9 @@ package ru.nsu.abramkin;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
