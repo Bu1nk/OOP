9c69cf18bc68655898470326c1045ecf6b492c5f
Bu1nk <Bu1nk@users.noreply.github.com>
2025-04-11 08:26:51 +0000

    Deploying to gh-pages from @ Bu1nk/OOP@5d24110cbe4ceb959c10bd8ec20e3e0d4c4a2b06 🚀

---
+++ Task_2_3_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.abramkin"},{"l":"ru.nsu.abramkin.controller"},{"l":"ru.nsu.abramkin.model"}];updateSearchResults();
